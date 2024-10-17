8d70ec10075898ef9fc79ecf5d256b92c258beba
David MANZI <manzidavid093@gmail.com>
2024-10-17 09:06:29 +0200

    ...

--- server.js
+++ server.js
@@ -1,4 +1,8 @@
 const express = require('express');
+const { param } = require('express-validator');
+const xss = require('xss-clean');
+const helmet = require('helmet');
+const rateLimit = require('express-rate-limit');
 // const https = require('https'); //
 // const fs = require('fs'); //
 const db_conn = require('./db');
@@ -6,6 +10,24 @@ const path = require('path');
 const app = express();
 
 const port = process.env.PORT || 10000;
+
+const limiter = rateLimit({
+    windowMs: 15 * 60 * 1000,
+    max: 100
+});
+
+app.use(limiter);
+
+app.use(xss());
+
+app.use(helmet());
+
+if (app.get('env') === 'production') {
+    app.use((err, req, res, next) => {
+        console.error(err.stack);
+        res.status(500).send('Something went worng!');
+    });
+}
 
 // const privateKey = fs.readFileSync(path.join(__dirname, 'https_certs/key.pem'), 'utf8'); //
 // const certificate = fs.readFileSync(path.join(__dirname, 'https_certs/cert.pem'), 'utf8'); //
@@ -24,7 +46,13 @@ app.get('/hymns', async (req, res) => {
     }
 });
 
-app.get('/hymns/:id', async (req, res) => {
+app.get('/hymns/:id', [
+    param('id').isInt().withMessage('ID must be an integer');
+] ,async (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+        return res.status(400).json({ errors: errors.array() });
+    }
     try {
         const { id } = req.params;
         const result = await db_conn.query('SELECT * FROM Hymns WHERE id = $1', [id]);
